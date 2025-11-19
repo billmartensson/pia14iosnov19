99092185995791067503bbe2b2e29c89ba2a6953
Bill Mårtensson <bill@magictechnology.se>
2025-11-19 14:13:14 +0100

    pia14

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -11,6 +11,14 @@ struct ContentView: View {
     
     @State var mynumber: Int = 0
     
+    func addnumbertext(nrtext : String) -> Int {
+        let trimmed = nrtext.trimmingCharacters(in: .whitespacesAndNewlines)
+        let number = Int(trimmed) ?? 0
+        let timestwo = number * 2
+        return timestwo
+    }
+    
+    
     
     var body: some View {
         VStack {
@@ -18,18 +26,57 @@ struct ContentView: View {
             Text("\(mynumber)")
                 .font(.largeTitle)
             
+            
             HStack {
                 Button("PLUS") {
                     mynumber += 1
                 }
-
                 Button("MINUS") {
                     mynumber -= 1
                 }
+                
+                Button("RESET") {
+                    mynumber = 0
+                }
+                
+                Button("ADD 5") {
+                    mynumber += 5
+                    if mynumber > 20 {
+                        mynumber = 0
+                    }
+                }
             }
-            .padding(20)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.yellow)
+            .cornerRadius(24)
+            .shadow(radius: 8)
+            .padding(.top, 100)
+            .padding(.bottom, 50)
             
-
+            Button("ABC") {
+                mynumber = 50
+            }
+            
+            Button(action: {
+                mynumber = 100
+            }) {
+                VStack {
+                    Text("ABC")
+                    Text("XYZ")
+                }
+                .padding()
+                .background(Color("Fancyred"))
+            }
+            
+            Image("frog")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 300)
+                .clipped()
+                .background(Color.red)
+                .cornerRadius(50)
+            
         }
         .padding()
     }
