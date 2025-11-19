90920ca01a2f50a4f410b2feb87cbd110c6149c4
Bill Mårtensson <bill@magictechnology.se>
2025-11-19 10:32:14 +0100

    Fixa frukt

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Kiwi")
+            Text("Banan")
             Text("Mera text")
             Text("Rad tre")
         }
