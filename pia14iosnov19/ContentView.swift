e046aa2ca5024da1ba23b88620c5bd134c17844d
Bill Mårtensson <bill@magictechnology.se>
2025-11-19 10:28:16 +0100

    Mer nya

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -22,6 +22,8 @@ struct ContentView: View {
             }
             .padding()
             .background(Color.cyan)
+            
+            Text("Mer nya saker")
         }
         .padding()
     }
