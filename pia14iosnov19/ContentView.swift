f78b587cf90a6a08c5fa1036c5ed00aa8655307f
Bill Mårtensson <bill@magictechnology.se>
2025-11-19 09:49:47 +0100

    Ändra frukt

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Banan")
+            Text("Kiwi")
             Text("Mera text")
             Text("Rad tre")
         }
