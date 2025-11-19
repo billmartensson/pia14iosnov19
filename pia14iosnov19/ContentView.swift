dd49d4405f5ff676aee7a7bb98969eeacd9e685d
Bill Mårtensson <bill@magictechnology.se>
2025-11-19 09:13:32 +0100

    Initial Commit

---
+++ pia14iosnov19/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pia14iosnov19
+//
+//  Created by BillU on 2025-11-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
