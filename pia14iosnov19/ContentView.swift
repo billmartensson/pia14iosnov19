d8a3a07f50735262afc14ad012abaf22fe6fd16b
billmartensson <bill@magictechnology.se>
2025-11-19 09:41:55 +0100

    Update ContentView.swift

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Tjena")
+            Text("Hej hej hej")
             Text("Mera text")
         }
         .padding()
